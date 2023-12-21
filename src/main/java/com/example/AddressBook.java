7662e37eb63104f9802e0d016f84d0267e43a78b
Pranshu Srivastava <pranshusrivastava000@gmail.com>
2023-12-22 04:32:45 +0530

    UC2 created

---
+++ src/main/java/com/example/AddressBook.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import java.util.ArrayList;
+
+public class AddressBook {
+    public static ArrayList<Contact> ContactsList = new ArrayList<Contact>();
+
+    public static void addContact() {
+        Contact contact = Contact.main(null);
+        ContactsList.add(contact);
+        System.out.println("");
+    }
+
+    public static void getContacts() {
+        for (int i = 0; i < ContactsList.size(); i++) {
+            Contact contact = ContactsList.get(i);
+            System.out.println("Details of Person " + (i + 1));
+            System.out.println("First Name: " + contact.firstName);
+            System.out.println("Last Name: " + contact.lastName);
+            System.out.println("City: " + contact.city);
+            System.out.println("State: " + contact.state);
+            System.out.println("Phone Number: " + contact.phoneNumber);
+            System.out.println("Zip Code: " + contact.zip);
+            System.out.println("Email: " + contact.email);
+            System.out.println("");
+        }
+    }
+
+    public static void main(String[] args) {
+        addContact();
+        getContacts();
+        addContact();
+        getContacts();
+    }
+}
--- src/main/java/com/example/Contact.java
+++ src/main/java/com/example/Contact.java
@@ -16,7 +16,7 @@ public class Contact {
         this.email = email;
     }
 
-    public static void main(String[] args) {
+    public static Contact main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the First Name");
         String firstName = sc.nextLine();
@@ -34,14 +34,6 @@ public class Contact {
         String email = sc.nextLine();
 
         Contact contact = new Contact(firstName, lastName, city, state, zip, phoneNumber, email);
-
-        System.out.println("Address Book");
-        System.out.println("First Name: " + contact.firstName);
-        System.out.println("Last Name: " + contact.lastName);
-        System.out.println("City: " + contact.city);
-        System.out.println("State: " + contact.state);
-        System.out.println("Phone Number: " + contact.phoneNumber);
-        System.out.println("Zip Code: " + contact.zip);
-        System.out.println("Email: " + contact.email);
+        return contact;
     }
 }
