fe2654f22d11e9febb9599fb59b2adda747a63ea
Pranshu Srivastava <pranshusrivastava000@gmail.com>
2024-01-03 01:07:57 +0530

    changes in UC7

--- src/main/java/com/example/AddressBook.java
+++ src/main/java/com/example/AddressBook.java
@@ -1,8 +1,9 @@
 package com.example;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class AddressBook {
     public String name;
@@ -11,7 +12,7 @@ public class AddressBook {
         this.name = name;
     }
 
-    public Map<String, Contact> ContactsList = new HashMap<>();
+    public ArrayList<Contact> ContactsList = new ArrayList<Contact>();
     static Scanner sc;
     static {
         sc = new Scanner(System.in);
@@ -20,11 +21,15 @@ public class AddressBook {
     public void addContact() {
         Contact contact = Contact.createContact();
         String name = contact.firstName + " " + contact.lastName;
-        if (ContactsList.containsKey(name)) {
-            System.out.println("This person already exists");
+        List<Contact> matchingPersons = ContactsList.stream()
+                .filter(person -> (person.firstName + " " + person.lastName).equals(name))
+                .collect(Collectors.toList());
+        if (matchingPersons.size() != 0) {
+            System.out.println("Contact with this name already exists");
             return;
         }
-        ContactsList.put(name, contact);
+        ContactsList.add(contact);
+        System.out.println("\nContact added Succesfully\n");
     }
 
     public void getContacts() {
@@ -32,11 +37,11 @@ public class AddressBook {
             System.out.println("There are no contacts in the Address Book");
             return;
         }
-        int i = 1;
-        for (Map.Entry<String, Contact> contact : ContactsList.entrySet()) {
-            System.out.println("Details of Person " + (i));
-            showContact(contact.getValue());
-            i++;
+        for (int i = 0; i < ContactsList.size(); i++) {
+            Contact contact = ContactsList.get(i);
+            System.out.println("Details of Person " + (i + 1));
+            showContact(contact);
+
         }
     }
 
@@ -58,25 +63,32 @@ public class AddressBook {
         System.out.println("Enter the Last Name of the person to be searched");
         String LastName = sc.nextLine();
 
-        String name = FirstName + " " + LastName;
-
-        if (!ContactsList.containsKey(name)) {
-            System.out.println("There is no person with this name in the address book");
-            return;
+        boolean found = false;
+        for (Contact obj : ContactsList) {
+            if (obj.firstName.equals(FirstName) && obj.lastName.equals(LastName)) {
+                System.out.println("Enter the New First Name ");
+                obj.firstName = sc.nextLine();
+                System.out.println("Enter the New Last Name ");
+                obj.lastName = sc.nextLine();
+                System.out.println("Enter the New City ");
+                obj.city = sc.nextLine();
+                System.out.println("Enter the New State ");
+                obj.state = sc.nextLine();
+                System.out.println("Enter the New Phone Number ");
+                obj.phoneNumber = sc.nextLine();
+                System.out.println("Enter the New Zip Code ");
+                obj.zip = sc.nextLine();
+                System.out.println("Enter the New Email ");
+                obj.email = sc.nextLine();
+                found = true;
+                System.out.println("The updated details are:-");
+                showContact(obj);
+                break;
+            }
         }
-
-        System.out.println("Enter the New City ");
-        ContactsList.get(name).city = sc.nextLine();
-        System.out.println("Enter the New State ");
-        ContactsList.get(name).state = sc.nextLine();
-        System.out.println("Enter the New Phone Number ");
-        ContactsList.get(name).phoneNumber = sc.nextLine();
-        System.out.println("Enter the New Zip Code ");
-        ContactsList.get(name).zip = sc.nextLine();
-        System.out.println("Enter the New Email ");
-        ContactsList.get(name).email = sc.nextLine();
-        System.out.println("The updated details are:-");
-        showContact(ContactsList.get(name));
+        if (!found) {
+            System.out.println("There is no person with this name in the address book");
+        }
     }
 
     public void deleteContact() {
@@ -84,13 +96,13 @@ public class AddressBook {
         String FirstName = sc.nextLine();
         System.out.println("Enter the Last Name of the person to be deleted");
         String LastName = sc.nextLine();
-        String name = FirstName + " " + LastName;
-        if (!ContactsList.containsKey(name)) {
+        String targetName = FirstName + " " + LastName;
+        boolean flag = ContactsList.removeIf(person -> (person.firstName + " " + person.lastName).equals(targetName));
+
+        if (!flag)
             System.out.println("There is no person with this name in the address book");
-            return;
-        }
-        ContactsList.remove(name);
-        System.out.println("Contact deleted Successfully");
+        else
+            System.out.println("Contact Deleted Succesfully");
     }
 
     public static AddressBook createAddressBook(String name) {
